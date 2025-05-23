7a179a2a5414da7e354ffe8cf49235d32924dfc5
Reis <aglemar.reis@gmail.com>
2025-05-23 11:18:44 -0300

    feat: realização da atividade 06

--- Grupo1Atv-python-atividades/06-operadores-matematicos.py
+++ Grupo1Atv-python-atividades/06-operadores-matematicos.py
@@ -7,3 +7,13 @@ print(a + b, a - b, a * b, a / b, a ** b, a // b, a % b)
 Crie quatro novas variaveis de numeros inteiros, some elas, 
 calcule a média e exiba o resultado na tela
 """
+
+primeiroElemento = 10
+segundoElemento = 20
+terceiroElemento = 30
+quartoElemento = 40
+
+soma = primeiroElemento + segundoElemento + terceiroElemento + quartoElemento
+media = soma / 4
+
+print(f"Média: {media:.2f}")
