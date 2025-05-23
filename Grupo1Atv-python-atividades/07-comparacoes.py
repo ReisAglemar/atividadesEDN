064c92fa5900b61a2247e51fe9ba2a2f8a170d00
Reis <aglemar.reis@gmail.com>
2025-05-23 11:24:34 -0300

    feat: realização da atividade 07

--- Grupo1Atv-python-atividades/07-comparacoes.py
+++ Grupo1Atv-python-atividades/07-comparacoes.py
@@ -1,5 +1,9 @@
 x = 10
 y = 15
 print(x > y, x < y, x == y, x != y)
+print("'x' é maior que o 'y'?", x > y)
+print("'x' é menor que o 'y'?", x < y)
+print("'x' é igual a 'y'?", x == y)
+print("'x' é diferente de 'y'?", x != y)
 
 #Compare dois números e exiba uma frase na tela de acordo com a comparação escolhida
