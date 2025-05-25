af5d7fb2ea534cca4c0cbf0d4fa25ba5a6b8455b
Reis <aglemar.reis@gmail.com>
2025-05-25 11:10:14 -0300

    feat: realização da atividade do grupo 2, verificador de senhas fortes

---
+++ Grupo2Atv-11-atividades/verificadorSenhas.py
@@ -0,0 +1,25 @@
+def verficaSenha(senha):
+    if len(senha) > 7:
+        for c in senha:
+            if c.isnumeric():
+                return True
+        print("Sua senha deve ter pelo um caractere numérico")
+        return False
+    else:
+        print("Sua senha deve ter pelo menos 8 caracteres")
+        return False
+
+def main():
+    while True:
+        senha = input("Informe sua senha para verificar se é forte, ou digite 'sair' para sair: ")
+
+        if senha.lower() == "sair":
+            print("Programa encerrado!")
+            break
+
+        if verficaSenha(senha):
+            print("Sua senha é considerada uma senha válida!")
+            print(f"Sua senha: {senha}")
+            break
+
+main()
